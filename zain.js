ebe2b9677fc0471f83ecc757e94afff0cc7bcb93
zain <shahzain354@gmail.com>
2024-12-24 12:53:48 +0000

    message

---
+++ zain.js
@@ -0,0 +1,122 @@
+let day = "tuesday";
+switch (day){
+    case "monday":
+        console.log("to day is monday")
+        break;
+        case "tuesday":
+            console.log("to day is tuesday")
+            break;
+            case "wensday":
+            console.log("to day is")
+            default:
+                console.log("look foreward")
+}
+
+
+//=====================================================================================================
+
+
+
+let marks = 50;
+ switch (true) {
+    case (marks >=90 && marks <=100):
+        console.log("student get A+ gread");
+        break;
+        case (marks>=75 && marks <=90):
+            console.log("student get A gread");
+            break;
+            case (marks>=60 && marks<=75):
+            console.log("student get B gread")
+            break;
+            case(marks>=45 && marks<=60 ):
+            console.log("student get C gread ");
+            break;
+            default:
+                console.log("student not performed well")
+ }
+
+
+//=====================================================================================================
+
+
+
+
+
+ let drink = "sting";
+ switch (true){
+    case(drink === "coc"|| drink=== "fanta") :
+    console.log("bring to home");
+    break;
+    case(drink==="pepsi" || drink==="sting") :
+    console.log("oky bring");
+    break;
+    default:
+        console.log("bring all except dew")
+
+}
+
+
+
+
+//====================================================================================================
+
+
+
+
+
+
+let num = 70;
+if (num>=90){
+    console.log("you get A+ gread")
+}else if(num>=75){
+    console.log("you get A gread")
+}else if(num>=60){
+    console.log("you get B gread")
+}else if(num>=50){
+    console.log("you get c gread")
+}else{
+    console.log("you are not qualify")
+}
+
+
+
+
+
+
+
+//==========================================================================================================
+
+
+
+
+
+
+
+// for(let i=0; i<10; i++) { 
+//     console.log(i)
+// }
+
+
+
+
+//=======================================================
+
+
+
+// let colors =[ "red"];
+// for (let i = o ; i < colors.length; i++){
+//     console.log(colors[i]);
+// }
+
+
+
+
+//================================================
+
+
+
+// let i = 0;
+// while (i<=20){
+//     console.log(i);
+//     i++;
+// }
